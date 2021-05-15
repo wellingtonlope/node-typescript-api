8f0a26721c25295b5b060d15ae4abd8d9d689934
Wellington Lopes <wellington.lopes.tw@gmail.com>
2021-05-14 22:31:03 -0400

    chapter 5

---
+++ src/services/auth.ts
@@ -0,0 +1,34 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import config from 'config';
+import { User } from '@src/models/user';
+
+export interface DecodeUser extends Omit<User, '_id'> {
+	id: string;
+}
+
+export default class AuthService {
+	public static async hashPassword(
+		password: string,
+		salt = 10
+	): Promise<string> {
+		return await bcrypt.hash(password, salt);
+	}
+
+	public static async comparePasswords(
+		password: string,
+		hashPassword: string
+	): Promise<boolean> {
+		return await bcrypt.compare(password, hashPassword);
+	}
+
+	public static generateToken(payload: object): string {
+		return jwt.sign(payload, config.get('App.auth.key'), {
+			expiresIn: config.get('App.auth.tokenExpiresIn'),
+		});
+	}
+
+	public static decodeToken(token: string): DecodeUser {
+		return jwt.verify(token, config.get('App.auth.key')) as DecodeUser;
+	}
+}
